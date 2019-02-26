b7e845de9bce9c5a0c4b6336a6f4858b614b6645
CarstenBehrends <c.behrends@flow4.com>
2019-02-26 08:59:24 +0100

    test

---
+++ server/db.js
@@ -0,0 +1,4 @@
+var mongoose = require('mongoose');
+var db_user='cabeDevUser';
+var db_pass='0qDgiOUI6SKiQRMC';
+mongoose.connect('mongodb+srv://cabeDevUser:0qDgiOUI6SKiQRMC@clusterdevelop-aba67.mongodb.net/test?retryWrites=true');
